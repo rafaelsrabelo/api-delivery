1e0a67ef0cbe7ba23aee834af66c38e87cddb0aa
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-12-19 20:41:20 -0300

    chore(package.json): add "@nestjs/config" dependency to enable configuration management feat(app.module.ts): add ConfigModule to load environment variables and validate them using envSchema feat(env.ts): create envSchema to define the shape and validation rules for environment variables feat(main.ts): use ConfigService to get the port from the environment variables and listen on that port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccounController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccounController],
   providers: [PrismaService],
 })
