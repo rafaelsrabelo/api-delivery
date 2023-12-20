d0d17d51258672a318f7e4f783b8cc2518c8e00c
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-12-20 13:39:13 -0300

    feat(auth): add CurrentUser decorator to extract user payload from request in controllers fix(jwt.strategy): rename tokenSchema to tokenPayloadSchema for clarity fix(create-order.controller): add CurrentUser decorator to handle method to extract user payload from request

--- src/controllers/create-order.controller.ts
+++ src/controllers/create-order.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
+import { Controller, Post, Req, UseGuards } from '@nestjs/common'
+import { Request } from 'express'
+import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth-guard'
+import { UserPayload } from 'src/auth/jwt.strategy'
 
 @Controller('/api/v1/orders')
 @UseGuards(JwtAuthGuard)
 export class CreateOrderController {
-  constructor() {
-    console.log('a')
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
     return 'ok'
   }
 }
