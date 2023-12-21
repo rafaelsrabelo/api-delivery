a867c112596cc8f101e47bf6fa0c51fa81f8b299
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-12-20 21:53:51 -0300

    fix(fetch-recent-orders.controller.ts): add query parameter validation for 'page' to ensure it is a positive number feat(fetch-recent-orders.controller.ts): add pagination support to fetch recent orders endpoint to limit the number of results returned per page and skip the appropriate number of records based on the page number

--- src/controllers/fetch-recent-orders.controller.ts
+++ src/controllers/fetch-recent-orders.controller.ts
@@ -1,6 +1,20 @@
-import { Controller, Get, UseGuards } from '@nestjs/common'
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
 import { JwtAuthGuard } from 'src/auth/jwt-auth-guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const ITEMS_PER_PAGE = 10
 
 @Controller('/api/v1/orders')
 @UseGuards(JwtAuthGuard)
@@ -8,8 +22,10 @@ export class FetchRecentOrdersController {
   constructor(private prisma: PrismaService) {}
 
   @Get()
-  async handle() {
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
     const orders = await this.prisma.order.findMany({
+      take: ITEMS_PER_PAGE,
+      skip: (page - 1) * ITEMS_PER_PAGE,
       orderBy: {
         created_at: 'desc',
       },
