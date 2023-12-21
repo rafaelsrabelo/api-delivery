79e218e20327bd33f822805af8dcd47d1c920ec4
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-12-21 09:47:03 -0300

    fix(fetch-recent-orders.controller.ts): fix query parameter validation to correctly handle page and status parameters feat(fetch-recent-orders.controller.ts): add support for filtering orders by status parameter in the query

--- src/controllers/fetch-recent-orders.controller.ts
+++ src/controllers/fetch-recent-orders.controller.ts
@@ -5,13 +5,24 @@ import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
 
 const pageQueryParamSchema = z
-  .string()
-  .optional()
-  .default('1')
-  .transform(Number)
-  .pipe(z.number().min(1))
+  .object({
+    page: z
+      .string()
+      .optional()
+      .default('1')
+      .transform(Number)
+      .refine((val) => val >= 1, {
+        message: 'Page number must be greater than or equal to 1',
+      }),
+    status: z
+      .string()
+      .optional()
+      .transform((val) => val && val.toLowerCase()),
+  })
+  .refine((val) => typeof val === 'object' && val !== null)
 
 const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
 type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 
 const ITEMS_PER_PAGE = 10
@@ -22,13 +33,26 @@ export class FetchRecentOrdersController {
   constructor(private prisma: PrismaService) {}
 
   @Get()
-  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+  async handle(@Query(queryValidationPipe) params: PageQueryParamSchema) {
+    const { page, status } = params
+
+    let where = {}
+
+    if (status) {
+      where = {
+        status: {
+          equals: status,
+        },
+      }
+    }
+
     const orders = await this.prisma.order.findMany({
       take: ITEMS_PER_PAGE,
       skip: (page - 1) * ITEMS_PER_PAGE,
       orderBy: {
         created_at: 'desc',
       },
+      where,
     })
 
     return { orders }
